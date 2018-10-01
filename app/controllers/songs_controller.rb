3be2d52765380d196056af143b19323d7c5915ff
Obafemi Fadairo <ofpupp@princeton.edu>
2018-10-01 21:35:58 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -49,7 +49,7 @@ class SongsController < ApplicationController
   def upload
     # binding.pry
     CSV.foreach(params[:file].path, headers: true) do |x|
-      binding.pry
+      # binding.pry
       Song.create(title: x[0], artist_name: x[1])
     end
     redirect_to songs_path
